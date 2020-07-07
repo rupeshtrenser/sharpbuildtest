ff5f97166941b80f95856bfb0d94bdcf52205dc2
Rupesh Sreeraman <rupesh.sreeraman@trenser.com>
2020-07-07 12:52:50 +0530

    added resize test

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const sharp = require('sharp');
-
+module.exports = {
+ ImageResize:function()
+{
+	return new Promise((resolve, reject) => {
 sharp("input.jpg")
   .resize(320, 240)
-  .toFile('output.webp', (err, info) => { console.log(info); });
-console.log("finished")
+  .toFile('output.webp', (err, info) => { console.log(info); resolve(info.width);});
+	});
+}
+};
+
---
+++ test/imgresize-test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var expect = require('chai').expect;
+var indx = require('../index')
+
+describe('Math', function() {
+    
+	describe('#ImageResize()', function() {
+        it('Should return width 320', function() {
+			
+			indx.ImageResize().then(imgsz=>
+			{expect(imgsz).to.be.equal(320);}
+			);
+		
+        });
+    });
+});
